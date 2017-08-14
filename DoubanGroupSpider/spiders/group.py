945d67ca8bc0d796d05550c2318ea9cea19a09cf
davidtangjw <davidtangjw@pptv.com>
2017-08-14 17:53:10 +0800

    趴去topic页面

--- DoubanGroupSpider/spiders/group.py
+++ DoubanGroupSpider/spiders/group.py
@@ -12,6 +12,23 @@ class DoubanGroupSpider(scrapy.Spider):
     name = "DBGroup"
     pageCount = 1
 
+    custom_settings = {
+        'CONCURRENT_REQUESTS': 1,
+        'DOWNLOAD_DELAY': 0.5,
+        'DEFAULT_REQUEST_HEADERS': {
+            "Host": "www.douban.com",
+            "Connection": "keep-alive",
+            "Pragma": "no-cache",
+            "Cache-Control": "no-cache",
+            "Upgrade-Insecure-Requests": "1",
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
+            "Cookie": 'll="108296"; bid=7X-KQZ2FHSI; ps=y; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1502690883%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3DRV36u6KGO1E5mDJ6NBxZphOJIrTCZtqFz5B2WLRl3WKQ7yRvJAu4mWxAIGLoMbLT9bsJbmvEatB79i9HW4KrW_%26ck%3D2912.3.79.196.446.380.343.791%26shh%3Dwww.baidu.com%26sht%3Dbaidu%26wd%3D%26eqid%3D9e17334800011425000000055991121c%22%5D; push_noty_num=0; push_doumail_num=0; __utmt=1; ap=1; as="https://www.douban.com/group/topic/105197785/"; _pk_id.100001.8cb4=190ee1ed74ea681f.1502275096.4.1502690948.1502682917.; _pk_ses.100001.8cb4=*; __utma=30149280.1750424429.1502275097.1502679582.1502690884.4; __utmb=30149280.13.5.1502690948183; __utmc=30149280; __utmz=30149280.1502679582.3.3.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=30149280.11931',
+        }
+    }
+
     def start_requests(self):
 
         urls = []
@@ -24,19 +41,7 @@ class DoubanGroupSpider(scrapy.Spider):
         for url in urls:
             yield scrapy.Request(
                 url=url, 
-                callback=self.parse, 
-                headers={
-                    "Host": "www.douban.com",
-                    "Connection": "keep-alive",
-                    "Pragma": "no-cache",
-                    "Cache-Control": "no-cache",
-                    "Upgrade-Insecure-Requests": "1",
-                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/59.0.3071.115 Safari/537.36",
-                    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8",
-                    "Accept-Encoding": "gzip, deflate, br",
-                    "Accept-Language": "zh-CN,zh;q=0.8,en;q=0.6",
-                    "Cookie": "bid=RLMIjL-JawY; ps=y; ap=1; ct=y; push_noty_num=0; push_doumail_num=0; _pk_ref.100001.8cb4=%5B%22%22%2C%22%22%2C1502110071%2C%22https%3A%2F%2Fwww.baidu.com%2Flink%3Furl%3Dy7gt9gU5EPJAXCTkTG-M6RSYIPeOCCsrwwjkJKCUgbecsLC60PoYayvZWaY3aZgioIpU208D9tPMpCSViQm5Ja%26wd%3D%26eqid%3D893d05690002357a0000000459886150%22%5D; _pk_id.100001.8cb4=bc57e396c910ab8d.1501396277.4.1502110237.1501511199.; _pk_ses.100001.8cb4=*; __utma=30149280.1999551137.1501396277.1501510334.1502110072.4; __utmb=30149280.24.5.1502110238123; __utmc=30149280; __utmz=30149280.1502110072.4.4.utmcsr=baidu|utmccn=(organic)|utmcmd=organic; __utmv=30149280.11931; __yadk_uid=jIhNVXdNXUmTJTiZsJr58yc98TNCGaIQ",
-                }
+                callback=self.parse
             )
 
     def parse(self, response):
@@ -47,6 +52,21 @@ class DoubanGroupSpider(scrapy.Spider):
                     'author':  tr.css('td')[1].css('a::text').extract_first(),
                 } '''
             ''' 获取链接地址 '''
-            yield {
-                'link': tr.xpath('td[@class="title"]/a/@href').extract_first(),
-            }
+            yield response.follow(tr.xpath('td[@class="title"]/a/@href').extract_first(), self.topicParse) 
+    
+    def topicParse(self, response):
+        ''' 取出图片信息 '''
+        topicFigures = []
+        for topicFigure in  response.xpath('//div[@id="link-report"]/div[@class="topic-content"]/div[@class="topic-figure cc"]'):
+            topicFigures.append({
+                'img': topicFigure.xpath('img/@src').extract_first(),
+                'title': topicFigure.xpath('span/text()').extract_first(default="not found")
+            })
+
+
+        yield {
+            'topic-content': '---'.join(response.xpath('//div[@id="link-report"]/div[@class="topic-content"]/p/text()').extract()),
+            'fav-num': response.xpath('//div[@class="sns-bar-fav"]/span[@class="fav-num"]/a/text()').extract_first(default="0"),
+            'topic-figure': topicFigures
+        }
+        
